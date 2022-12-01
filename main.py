67d5aee3e065e191e36b7b0d083f6a9a36e3611b
JustGimli <aleksnermer@gmail.com>
2022-12-01 17:47:08 +0300

    remove trash

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
             N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))
             if 22 >y and y > 0 and x > 0 and 80 > x and D >  z[o]:
                 z[o] = D
-                b[o] = ".,-~:;=!*#$&"[N if N > 0 else 0]
+                b[o] = ".,-~:;=!*#$@"[N if N > 0 else 0]
     print('\x1b[H', end='')
     for k in range(1761):
         print((b[k] if k % 80 else '\n'), end='')
